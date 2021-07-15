3278ae59c7bdcc17097137faba406be6e8c9986e
SamiraFreitas <73719899+SamiraFreitas@users.noreply.github.com>
2021-07-15 11:53:15 -0300

    atualizacao

--- fun_lista.py
+++ fun_lista.py
@@ -1,32 +1,34 @@
 
 
-class Grafo_Matriz:
+class Grafo_Lista:
     def __init__(self, arquivo):
         (self.num_vertices, self.num_arestas) = self.tamanhos(arquivo)
-        self.grafo = self.converte_matriz(arquivo)
+        self.grafo = self.converte_lista(arquivo)
         (self.maior_grau, self.menor_grau, self.media_grau, self.frequencia) = self.definir_graus()
-        self.busca_largura_matriz(0)
-        self.busca_profundidade_matriz(0)
+        self.busca_largura_lista(0)
+        self.busca_profundidade_lista(0)
         (self.componentes_conexas, self.num_conexa) = self.conexidade()
 
-    def converte_matriz(self, arquivo):
-        "Converte o grafo do arquivo .dat em uma matriz de adjacencias"
-        matriz = [[0 for _ in range(self.num_vertices)] for _ in range(self.num_vertices)]
+    def converte_lista(self, arquivo):
+        "Converte o grafo do arquivo .dat em uma lista de adjacencias"
+        lista = []
+        for i in range(self.num_vertices):
+            lista.append([])
 
         for linha in arquivo:
             conteudo = linha.split(' ')
             v1 = int(conteudo[0])
             v2 = int(conteudo[1])
             a = int(conteudo[2])
-            matriz[v1][v2] = a
-            matriz[v2][v1] = a
-        return matriz
+            lista[v1].append((v2, a))
+            lista[v2].append((v1, a))
+        return lista
 
     def tamanhos(self, arquivo):
         "retorna o numero de vertices e arestas no grafo"
         for linha in arquivo:
             conteudo = linha.split(' ')
-            return ( int(conteudo[0]), int(conteudo[1]))
+            return (int(conteudo[0]), int(conteudo[1]))
 
     def definir_graus(self):
         "calcula o vertice com maior grau, o com menor grau, o grau medio do grafo"
@@ -38,67 +40,61 @@ class Grafo_Matriz:
 
         for i in range(self.num_vertices):
             aux = 0
-            for j in range(self.num_vertices):
-                if self.grafo[i][j] != 0:
-                    aux= aux +1
-            distribuicao[aux] = distribuicao[aux] + 1
-            if maior[1] < aux:
+            distribuicao[len(self.grafo[i])] = distribuicao[len(self.grafo[i])] + 1
+            if maior[1] < len(self.grafo[i]):
                 maior[0] = i
-                maior[1] = aux
-            if menor[1] > aux:
+                maior[1] = len(self.grafo[i])
+            elif menor[1] > len(self.grafo[i]):
                 menor[0] = i
-                menor[1] = aux
-            soma = soma + aux
+                menor[1] = len(self.grafo[i])
+            soma = soma + len(self.grafo[i])
         for i in range(self.num_vertices):
             if distribuicao[i] != 0:
                 frequencia.append((i, distribuicao[i] / self.num_vertices))
-        media = float(soma)/float(self.num_vertices)
-        return(maior, menor, media, frequencia)
+        media = float(soma) / float(5)
+        return (maior, menor, media, frequencia)
 
-    def busca_largura_matriz(self, s):
+    def busca_largura_lista(self, s):
         "Escreve em um arquivo .txt saida o nivel de cada vertice na arvore, ..."
         "... considerando o caminho percorrido na busca em largura."
         arquivo2 = open('nivel_largura.txt', 'w')
-
-        desc = [0 for i in range(len(self.grafo))]
+        desc = [0 for i in range(self.num_vertices)]
         Q = [s]
         R = [s]
         desc[s] = 1
-        ordem = [-1 for i in range(len(self.grafo))]
+        ordem = [-1 for i in range(self.num_vertices)]
         ordem[s] = 0
 
         while len(Q) != 0:
             u = Q.pop(0)
-            for v in range(len(self.grafo[u])):
-                if self.grafo[u][v] != 0 and desc[v] == 0:
+            for (v, a) in self.grafo[u]:
+                if desc[v] == 0:
                     Q.append(v)
                     R.append(v)
                     desc[v] = 1
                     if ordem[v] == -1:
                         ordem[v] = ordem[u] + 1
-
         for i in range(len(ordem)):
             if ordem[i] != -1:
                 saidas = str(i) + ':' + str(ordem[i]) + '\n'
                 arquivo2.write(saidas)
-
         arquivo2.close()
 
-    def busca_profundidade_matriz(self, s):
+    def busca_profundidade_lista(self, s):
         "Escreve em um arquivo .txt saida o nivel de cada vertice na arvore, ..."
         "considerando o caminho percorrido na busca em profundidade"
         arquivo3 = open('nivel_profundidade.txt', 'w')
-        desc = [0 for i in range(len(self.grafo))]
+        desc = [0 for i in range(self.num_vertices)]
         S = [s]
         R = [s]
         desc[s] = 1
-        ordem = [-1 for i in range(len(self.grafo))]
+        ordem = [-1 for i in range(self.num_vertices)]
         ordem[s] = 0
         while len(S) != 0:
             u = S[-1]
             desempilhar = True
-            for v in range(len(self.grafo[u])):
-                if self.grafo[u][v] != 0 and desc[v] == 0:
+            for (v, a) in self.grafo[u]:
+                if desc[v] == 0:
                     desempilhar = False
                     S.append(v)
                     R.append(v)
@@ -123,8 +119,8 @@ class Grafo_Matriz:
         comp[s] = 0
         while len(Q) != 0:
             u = Q.pop(0)
-            for v in range(self.num_vertices):
-                if self.grafo[u][v] != 0 and desc[v] == 0:
+            for (v, a) in self.grafo[u]:
+                if desc[v] == 0:
                     Q.append(v)
                     R.append(v)
                     desc[v] = 1
@@ -136,7 +132,7 @@ class Grafo_Matriz:
         componente = [1 for i in range(self.num_vertices)]
         t = []
         k = 0
-        for i in range(len(self.grafo)):
+        for i in range(self.num_vertices):
             bit = 0
             if componente[i] != 0:
                 busca = self.busca_largura(componente, i)
--- main.py
+++ main.py
@@ -1,48 +1,47 @@
+import os
 import fun_matriz
-entrada = input('Informe o nome o arquivo:')
-entrada= entrada+'.dat'
+import fun_lista
+
+ch = False
+while(ch == False):
+    entrada1 = input('\n-Informe o nome do arquivo: ')
+    entrada = entrada1 + '.txt'
+    ch = os.path.exists(entrada)
+    if ch == False:
+        entrada = entrada1 + '.dat'
+        ch = os.path.exists(entrada)
+    if ch == False:
+        print("\nInfelizmente não foi possivel encontrar esse arquivo, tente novamente com um nome valido")
+        espera = input('\t...aperte enter para continuar...')
 arquivo = open(entrada, 'r')
 
-print('\t','Informe a escolha de representação:')
-bit = input('\t\t(1 - LISTA DE ADJACENCIAS || 2- MATRIZ DE ADJACENCIAS ) -> ')
-bit = int(bit)
+print('\tEscolha o tipo de representação do grafo:')
+chave = input('\t\t(1 para lista de adjacencias  /  2 para matriz de adjacencias) -> ')
+chave = int(chave)
 
-if bit == 1:
-    "saida= fun_lista.Grafo_Lista(arquivo)"
+print('\n\n-Grafo:')
+if chave == 1:
+    saida = fun_lista.Grafo_Lista(arquivo)
+    for item in saida.grafo:
+        print(item)
 else:
     saida = fun_matriz.Grafo_Matriz(arquivo)
-
+    for i in range(saida.num_vertices):
+        for j in range(saida.num_vertices):
+            print(" %d" % (saida.grafo[i][j]), end="")
+        print('\n')
 arquivo.close()
 
-
-
-print('-Grafo:\n\n')
-for i in range(saida.num_vertices):
-
-    for j in range(saida.num_vertices):
-        print(" %d" % (saida.grafo[i][j]), end="")
-    print('\n')
-
-
-print('\tNumero de vertices:', saida.num_vertices)
-print('\tNumero de arestas:', saida.num_arestas)
-print('\tMaior grau:', saida.maior_grau[1],' no vertice:', saida.maior_grau[0])
-print('\tMaior grau:', saida.menor_grau[1],' no vertice:', saida.menor_grau[0])
-print('\tMedia grau:', saida.media_grau)
-print('Componentes conexas:', saida.componentes_conexas)
+print('-Numero de vertices:', saida.num_vertices)
+print('-Numero de arestas:', saida.num_arestas)
+print('-Maior grau:', saida.maior_grau[1], '| | no vertice:', saida.maior_grau[0])
+print('-Menor grau:', saida.menor_grau[1], '| | no vertice:', saida.menor_grau[0])
+print('-Media grau:', saida.media_grau)
+print('-Frequencia relativa:')
+for (grau, freq) in saida.frequencia:
+    print('->Grau', grau, ': ', freq)
+print('-Componentes conexas:', saida.componentes_conexas)
 for item in saida.num_conexa:
-    print('-',item, 'vertices')
+    print('->', item, 'vertices')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+fim = input()
